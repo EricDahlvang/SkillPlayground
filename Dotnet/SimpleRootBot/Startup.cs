228000bb2b051987f0f71f61f735e57ec1966722
Eric Dahlvang <erdahlva@microsoft.com>
2021-08-25 13:08:51 -0500

    Add PVA exported bot

--- Dotnet/SimpleRootBot/Startup.cs
+++ Dotnet/SimpleRootBot/Startup.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -13,7 +14,6 @@ using Microsoft.Bot.Builder.Integration.AspNet.Core;
 using Microsoft.Bot.Builder.Integration.AspNet.Core.Skills;
 using Microsoft.Bot.Builder.Skills;
 using Microsoft.Bot.Connector.Authentication;
-using Microsoft.BotBuilderSamples.SimpleRootBot.Authentication;
 using Microsoft.BotBuilderSamples.SimpleRootBot.Bots;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
@@ -27,43 +27,9 @@ namespace Microsoft.BotBuilderSamples.SimpleRootBot
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // MvcOptions.EnableEndpointRouting = false;
-            // .AddJsonOptions(options => options.JsonSerializerOptions.WriteIndented = true)
-
-            //services
-            //  .AddMvc()
-            //  .SetCompatibilityVersion(CompatibilityVersion.Latest)
-            //  .AddNewtonsoftJson();
-
-            /*
-            services.AddMvcCore(option => option.EnableEndpointRouting = false)
-                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
-                .AddNewtonsoftJson(o => 
-                { 
-                    o.SerializerSettings.NullValueHandling = NullValueHandling.Ignore;
-                    o.SerializerSettings.Formatting = Formatting.Indented;
-                    o.SerializerSettings.DateFormatHandling = DateFormatHandling.IsoDateFormat;
-                    o.SerializerSettings.DateTimeZoneHandling = DateTimeZoneHandling.Utc;
-                    o.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Serialize;
-                    o.SerializerSettings.ContractResolver = new ReadOnlyJsonContractResolver();
-                    o.SerializerSettings.Converters = new List<JsonConverter> { new Iso8601TimeSpanConverter() };
-                 });
-
-            //      services.TryAddSingleton<IActionResultExecutor<JsonResult>, SystemTextJsonResultExecutor>();
-
-            // NewtonsoftJsonMvcCoreBuilderExtensions.AddNewtonsoftJson();
-            */
-
-            //services.Add(new ServiceDescriptor(
-            //    typeof(IActionResultExecutor<JsonResult>),
-            //    Type.GetType("Microsoft.AspNetCore.Mvc.Infrastructure.SystemTextJsonResultExecutor, Microsoft.AspNetCore.Mvc.Core"),
-            //    ServiceLifetime.Singleton));
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
-//#if NETCOREAPP3_0
-//            result.SerializerSettings = HttpHelper.BotMessageSerializerSettings as object;
-//#endif
+
 
             // Configure credentials
             services.AddSingleton<ICredentialProvider, ConfigurationCredentialProvider>();
@@ -72,7 +38,7 @@ namespace Microsoft.BotBuilderSamples.SimpleRootBot
             services.AddSingleton<SkillsConfiguration>();
 
             // Register AuthConfiguration to enable custom claim validation.
-            services.AddSingleton(sp => new AuthenticationConfiguration { ClaimsValidator = new AllowedSkillsClaimsValidator(sp.GetService<SkillsConfiguration>()) });
+            services.AddSingleton(sp => new AuthenticationConfiguration { ClaimsValidator = new AllowedSkillsClaimsValidator(sp.GetService<SkillsConfiguration>().Skills.Values.Select(s=>s.AppId).ToList()) });
 
             // Register the Bot Framework Adapter with error handling enabled.
             // Note: some classes use the base BotAdapter so we add an extra registration that pulls the same instance.
@@ -109,12 +75,7 @@ namespace Microsoft.BotBuilderSamples.SimpleRootBot
             app.UseDefaultFiles();
             app.UseStaticFiles();
 
-            // app.UseHttpsRedirection();
             app.UseMvc();
-
-            //app.UseRouting();
-
-            //app.UseEndpoints((endpoints) => endpoints.MapDefaultControllerRoute());
         }
     }
 }
